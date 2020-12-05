9dcc30453ffb7690cda904f76ecbd27ca1be9fbb
mjm288 <65255238+mjm288@users.noreply.github.com>
2020-12-05 19:31:59 +0000

    Add files via upload

---
+++ WindowsApp1/WindowsApp1/Equation Solver.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(101, 242)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(137, 59)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Quadratic"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(320, 242)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(137, 59)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Cubic"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(549, 242)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(137, 59)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Quartic"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Impact", 48.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Yellow
+        Me.Label1.Location = New System.Drawing.Point(174, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(437, 80)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Equation Finder"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("HoloLens MDL2 Assets", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(188, 119)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(405, 26)
+        Me.TextBox1.TabIndex = 4
+        Me.TextBox1.Text = "Please choose your desired equation:"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.MediumBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ WindowsApp1/WindowsApp1/Equation Solver.vb
@@ -0,0 +1,137 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(Sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Equation, strSum, strProduct As String
+        Dim a, b, Sum, Product As Integer
+
+        a = InputBox("Please enter alpha root:")
+        b = InputBox("Please enter beta root: ")
+
+        Sum = (a + b)
+        Product = (a * b)
+
+        MsgBox("Sum: " & Sum)
+        MsgBox("Product: " & Product)
+
+        If Sum < 0 Then
+            Sum = (Sum * -1)
+            strSum = (" + " & Sum & "x")
+        Else
+            strSum = (" - " & Sum & "x")
+        End If
+
+        If Product < 0 Then
+            Product = (Product * -1)
+            strProduct = (" - " & Product)
+        Else
+            strProduct = (" + " & Product)
+
+        End If
+
+        Equation = ("x^2" & strSum & strProduct & " = 0")
+        MsgBox("Equation: " & Equation)
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim Equation, strSum, strCombi, strProduct As String
+        Dim a, b, c, Sum, Combi, Product As Integer
+
+        a = InputBox("Please enter alpha root: ")
+        b = InputBox("Please enter beta root: ")
+        c = InputBox("Please enter gamma root: ")
+
+        Sum = (a + b + c)
+        Combi = ((a * b) + (a * c) + (b * c))
+        Product = (a * b * c)
+
+        MsgBox("Sum: " & Sum)
+        MsgBox("Combination of pairs: " & Combi)
+        MsgBox("Product: " & Product)
+
+        If Sum < 0 Then
+            Sum = (Sum * -1)
+            strSum = (" + " & Sum & "x^2")
+        Else
+            strSum = (" - " & Sum & "x^2")
+        End If
+
+        If Combi < 0 Then
+            Combi = (Combi * -1)
+            strCombi = (" - " & Combi & "x")
+        Else
+            strCombi = (" + " & Combi & "x")
+        End If
+
+        If Product < 0 Then
+            Product = (Product * -1)
+            strProduct = (" + " & Product)
+        Else
+            strProduct = (" - " & Product)
+        End If
+
+        Equation = ("x^3" & strSum & strCombi & strProduct & " = 0")
+        MsgBox("Equation: " & Equation)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim Equation, strSum, strCombiPair, strCombiTriple, strProduct As String
+        Dim a, b, c, d, Sum, CombiPair, CombiTriple, Product As Integer
+
+        a = InputBox("Please enter alpha root: ")
+        b = InputBox("Please enter beta root: ")
+        c = InputBox("Please enter gamma root: ")
+        d = InputBox("Please enter delta root: ")
+
+        Sum = (a + b + c + d)
+        CombiPair = ((a * b) + (a * c) + (a * d) + (b * c) + (b * d) + (c * d))
+        CombiTriple = ((a * b * c) + (a * b * d) + (d * c * b))
+        Product = (a * b * c * d)
+
+        MsgBox("Sum: " & Sum)
+        MsgBox("Combination of Pairs: " & CombiPair)
+        MsgBox("Combination of Triples: " & CombiTriple)
+        MsgBox("Product: " & Product)
+
+        If Sum < 0 Then
+            Sum = (Sum * -1)
+            strSum = (" + " & Sum & "x^3")
+        Else
+            strSum = (" - " & Sum & "x^3")
+        End If
+
+        If CombiPair < 0 Then
+            CombiPair = (CombiPair * -1)
+            strCombiPair = (" - " & CombiPair & "x^2")
+        Else
+            strCombiPair = (" + " & CombiPair & "x^2")
+        End If
+
+        If CombiTriple < 0 Then
+            CombiTriple = (CombiTriple * -1)
+            strCombiTriple = (" + " & CombiTriple & "x")
+        Else
+            strCombiTriple = (" - " & CombiTriple & "x")
+        End If
+
+        If Product < 0 Then
+            Product = (Product * -1)
+            strProduct = (" - " & Product)
+        Else
+            strProduct = (" + " & Product)
+        End If
+
+        Equation = ("x^4" & strSum & strCombiPair & strCombiTriple & strProduct & " = 0")
+        MsgBox("Equation: " & Equation)
+    End Sub
+
+
+
+End Class
